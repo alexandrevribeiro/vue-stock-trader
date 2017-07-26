31f008b8973285eb090d4b307cc2faf10cf9565d
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-07-26 00:04:24 -0300

    Setting up Vue-resource

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource;'
 import App from './App.vue';
 import { routes } from './routes';
 import store from './store/store';
@@ -8,8 +9,11 @@ Vue.filter('currency', (value) => {
   return `R$ ${value}`;
 });
 
-// Uses the vue-router middleware
+// Uses the vue-router and vue-resource middleware
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-a7bc6.firebaseio.com/';
 
 const router = new VueRouter({
   routes, // The "history" mode requires the server to always return the "index.html" page.
